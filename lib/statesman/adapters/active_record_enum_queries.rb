b169872af8fa7be91062f4b4208ddaa7b92c046f
Gautham Shankar <gautham@germ.io>
2014-12-16 12:49:29 +0530

    AR enum queries and spec

---
+++ lib/statesman/adapters/active_record_enum_queries.rb
@@ -0,0 +1,49 @@
+module Statesman
+  module Adapters
+    module ActiveRecordEnumQueries
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+
+      module ClassMethods
+        def in_state(*states)
+          where(state_inclusion_where(states), 
+                states.map { |state| transition_class.enum_for_state(state) })
+          # raise error ?
+        end
+
+        def not_in_state(*states)
+          where("NOT (#{state_inclusion_where(states)})",
+                states.map { |state| transition_class.enum_for_state(state) })
+          # raise error ?
+        end
+
+        private
+
+        def transition_class
+          raise NotImplementedError, "A transition_class method should be " \
+                                     "defined on the model"
+        end
+
+        def column_name
+          transition_class.enum_column
+        end
+
+        def initial_state
+          raise NotImplementedError, "An initial_state method should be " \
+                                     "defined on the model"
+        end
+
+        def state_inclusion_where(states)
+          if initial_state.to_s.in?(states.map(&:to_s))
+            "#{column_name} IN (?) OR " \
+            "#{column_name} IS NULL"
+          else
+            "#{column_name} IN (?) AND " \
+            "#{column_name} IS NOT NULL"
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/statesman/adapters/active_record_enum_queries_spec.rb
@@ -0,0 +1,83 @@
+require "spec_helper"
+
+describe Statesman::Adapters::ActiveRecordEnumQueries do
+  before do
+    prepare_enum_model_table
+  end
+
+  before do
+    EnumActiveRecordModel.send(:include, Statesman::Adapters::ActiveRecordEnumQueries)
+    EnumActiveRecordModel.class_eval do
+      def self.transition_class
+        EnumActiveRecordTransition
+      end
+
+      def self.initial_state
+        :initial
+      end
+    end
+  end
+
+  let!(:model) do
+    model = EnumActiveRecordModel.new
+    model.enum_state = 2
+    model.save!
+    model
+  end
+
+  let!(:other_model) do
+    model = EnumActiveRecordModel.new
+    model.enum_state = 3
+    model.save!
+    model
+  end
+
+  let!(:initial_state_model) { EnumActiveRecordModel.create }
+
+  let!(:returned_to_initial_model) do
+    model = EnumActiveRecordModel.new
+    model.enum_state = 3
+    model.save!
+    model.enum_state = 1
+    model.save!
+    model
+  end
+
+  describe ".in_state" do
+    context "given a single state" do
+      subject { EnumActiveRecordModel.in_state(:succeeded) }
+
+      it { is_expected.to include model }
+    end
+
+    context "given multiple states" do
+      subject { EnumActiveRecordModel.in_state(:succeeded, :failed) }
+
+      it { is_expected.to include model }
+      it { is_expected.to include other_model }
+    end
+
+    context "given the initial state" do
+      subject { EnumActiveRecordModel.in_state(:initial) }
+
+      it { is_expected.to include initial_state_model }
+      it { is_expected.to include returned_to_initial_model }
+    end
+  end
+
+  describe ".not_in_state" do
+    context "given a single state" do
+      subject { EnumActiveRecordModel.not_in_state(:failed) }
+      it { is_expected.to include model }
+      it { is_expected.not_to include other_model }
+    end
+
+    context "given multiple states" do
+      subject { EnumActiveRecordModel.not_in_state(:succeeded, :failed) }
+      it do
+        is_expected.to match_array([initial_state_model,
+                                    returned_to_initial_model])
+      end
+    end
+  end
+end
